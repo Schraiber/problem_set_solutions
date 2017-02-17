95d81e77863082e1d0c3793a3f40709af555951e
schraiber <jgschraiber@gmail.com>
2017-02-17 13:01:47 -0500

    added problem 3

---
+++ Problem_Set_1/problem_3.py
@@ -0,0 +1,20 @@
+friendDict = {}
+print "Input the name of a friend"
+name = raw_input()
+print "Input your friend's age"
+age = int(raw_input())
+friendDict[name] = age
+print "Your friend is named", name, "and is currently", friendDict[name], "they will be", friendDict[name]+5, "in 5 years"
+print "Input the name of a friend"
+name = raw_input()
+print "Input your friend's age"
+age = int(raw_input())
+friendDict[name] = age
+print "Your friend is named", name, "and is currently", friendDict[name], "they will be", friendDict[name]+5, "in 5 years"
+print "Input the name of a friend"
+name = raw_input()
+print "Input your friend's age"
+age = int(raw_input())
+friendDict[name] = age
+print "Your friend is named", name, "and is currently", friendDict[name], "they will be", friendDict[name]+5, "in 5 years"
+
