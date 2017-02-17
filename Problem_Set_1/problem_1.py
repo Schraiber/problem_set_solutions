fb9ebd33344f64dba8f1b86473428f152961eaf5
schraiber <jgschraiber@gmail.com>
2017-02-17 12:57:13 -0500

    added problems 1 and 2

---
+++ Problem_Set_1/problem_1.py
@@ -0,0 +1,7 @@
+print "Please input a number"
+theNumber = int(raw_input())
+twiceTheNumber = 2*theNumber
+halfTheNumber = theNumber/2
+theNumberPlus50 = theNumber + 50
+theNumberMinus50 = theNumber - 50
+print twiceTheNumber, halfTheNumber, theNumberPlus50, theNumberMinus50
---
+++ Problem_Set_1/problem_2.py
@@ -0,0 +1,13 @@
+theList = []
+print "Input a number"
+theList.append(int(raw_input()))
+print "Input a number"
+theList.append(int(raw_input()))
+print "Input a number"
+theList.append(int(raw_input()))
+print "Input a number"
+theList.append(int(raw_input()))
+sumOfList = sum(theList)
+newList = [theList[0]*theList[1], theList[2]*theList[3]]
+print "The sum of all your numbers is", sumOfList
+print "The new list is", newList
